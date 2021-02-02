b041167473ce5734877b6d17ffe0ccdac3aaa392
Rocco Orefice <rocco.orefice92@gmail.com>
2021-02-02 15:31:01 +0100

    Struttura html scss php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+    include __DIR__ . '/../partials/db.php'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-ajax-dischi</title>
+    <link rel="stylesheet" href="./dist/css/style.css">
+</head>
+<body>
+    
+<script src="./dist/js/main.js"></script>
+</body>
+</html>
+
